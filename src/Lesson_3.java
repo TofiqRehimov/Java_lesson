d5ce484217013a9e97fde0f76b2466bc3673f4d8
Tofq <tofiqrehimov@gmail.com>
2024-11-13 03:01:39 +0300

    new commit

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lesson_3 {
@@ -45,50 +46,128 @@ public class Lesson_3 {
 //        }
 //        else{ System.out.println("Sizin baliniz F -dir "); }
 
+        /// Fesili tapmaq
 
-        System.out.println("hansi ay oldugunu qeyd edin ");
-        Scanner sc = new Scanner(System.in);
-        var input = sc.next();
-         switch (input) {
-             case "Yanvar":
-                 System.out.println("Qis feslidir");
-                 break;
-             case "Fevral":
-                 System.out.println("Qis feslidir");
-                 break;
-             case "Mart":
-                 System.out.println("Qis feslidir");
-                 break;
-             case " Aprel":
-                 System.out.println("Yaz feslidir");
-                 break;
-             case "May":
-                 System.out.println("Yaz feslidir");
-                 break;
-             case " Iyun":
-                 System.out.println("Yaz feslidir");
-                break;
-             case "Iyul":
-                 System.out.println("Yay feslidir");
-                 break;
-             case "Avqust":
-                 System.out.println("Yay feslidir");
-                 break;
-             case "Sentyabr":
-                 System.out.println("Yay feslidir");
-                 break;
-             case "Oktyabr":
-                 System.out.println("Payiz feslidir");
-                 break;
-             case "Noyabr":
-                 System.out.println("Payiz feslidir");
-                 break;
-             case "Dekabr":
-                 System.out.println("Payiz feslidir");
-                 break;
-                 default:
-                     System.out.println("Duzgun qeyd etmemisiniz");
+//        System.out.println("hansi ay oldugunu qeyd edin ");
+//        Scanner sc = new Scanner(System.in);
+//        var input = sc.next();
+//         switch (input) {
+//             case "Yanvar":
+//                 System.out.println("Qis feslidir");
+//                 break;
+//             case "Fevral":
+//                 System.out.println("Qis feslidir");
+//                 break;
+//             case "Mart":
+//                 System.out.println("Qis feslidir");
+//                 break;
+//             case " Aprel":
+//                 System.out.println("Yaz feslidir");
+//                 break;
+//             case "May":
+//                 System.out.println("Yaz feslidir");
+//                 break;
+//             case " Iyun":
+//                 System.out.println("Yaz feslidir");
+//                break;
+//             case "Iyul":
+//                 System.out.println("Yay feslidir");
+//                 break;
+//             case "Avqust":
+//                 System.out.println("Yay feslidir");
+//                 break;
+//             case "Sentyabr":
+//                 System.out.println("Yay feslidir");
+//                 break;
+//             case "Oktyabr":
+//                 System.out.println("Payiz feslidir");
+//                 break;
+//             case "Noyabr":
+//                 System.out.println("Payiz feslidir");
+//                 break;
+//             case "Dekabr":
+//                 System.out.println("Payiz feslidir");
+//                 break;
+//                 default:
+//                     System.out.println("Duzgun qeyd etmemisiniz");
+//
+//         }
 
-         }
+
+        ///  Fevral ayinin 29 dan olan il
+
+//        System.out.println("Ay daxil edin: ");
+//        Scanner sc = new Scanner(System.in);
+//        int month = sc.nextInt();
+//        if(month % 4 ==0 && month % 400 ==0){
+//            System.out.println("Fevral ayi 29 -dir");
+//        }
+//        else{ System.out.println("Fevral ayi 29-dan deyil"); };
+
+        /// Heftenin gunleri
+
+//        System.out.println("Gun daxil et: ");
+//        Scanner sc = new Scanner(System.in);
+//        int days = sc.nextInt();
+//
+//        switch (days) {
+//        case 1: System.out.println("Sunday"); break;
+//        case 2: System.out.println("Monday"); break;
+//        case 3: System.out.println("Tuesday"); break;
+//        case 4: System.out.println("Wednesday"); break;
+//        case 5: System.out.println("Thursday"); break;
+//        case 6: System.out.println("Friday"); break;
+//        case 7: System.out.println("Saturday"); break;
+//        }
+
+        /// Pozitiv ve neqativ reqemler
+
+//        System.out.println("Reqem yaz");
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        if(num > 0){
+//            System.out.println("Musbet eded");
+//        }
+//        else if(num ==0 ){
+//            System.out.println("Eded 0-dir");
+//        }
+//        else
+//        {
+//            System.out.println("Eded menfi ededdir");
+//        };
+
+        /// Endirim faizi
+
+//        System.out.println("Mebleg daxil edin: ");
+//        Scanner sc = new Scanner(System.in);
+//        float input = sc.nextFloat();
+//        if( input>= 10 && input<=100) {
+//            float num = (input /100) *7;
+//            float num2 = input - num;
+//            System.out.println("Satis meblegi: "+
+//                    space + input +space + "Endirim  7 %  = " +
+//                    num + "azn" +space + "Yekun mebleg: "+ num2+ space +"azn");
+//
+//        }
+//        else if(input > 100 && input<=200) {
+//            float num = (input /100) *15;
+//            float num2 = input - num;
+//            System.out.println("Satis meblegi: "+
+//                    space + input +space + "Endirim  15 %  = " +
+//                    num + "azn" +space + "Yekun mebleg: "+ num2+ space +"azn");
+//        }
+//        else if(input > 200 ) {
+//            float num = (input /100) *22;
+//            float num2 = input - num;
+//            System.out.println("Satis meblegi: "+
+//                    space + input +space + "Endirim  22 %  = " +
+//                    num + "azn" +space + "Yekun mebleg: "+ num2+ space +"azn");
+//        }
+//        else {
+//            System.out.println("Size endirim dusmur");
+//        }
+
+
+//
     }
 }
---
+++ src/Lesson_4.java
@@ -0,0 +1,53 @@
+public class Lesson_4 {
+    public static void main(String[] args) {
+        int a = 3;
+//       int b = 5;
+//       a = a +2;
+//       b = b-2;
+//       System.out.println(a + " " + b);
+
+//        System.out.println("Eded daxil edin");
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        boolean num1 = ( num % 2 ==0);
+//        System.out.println("Cut eded " + num1);
+
+//        int a= 1;
+//        while (a <= 10){
+//            if(a %2 ==0){
+//                System.out.println(a);
+//            }
+//            a++;
+//
+//        }
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//
+//        for (int a = 1; a <= 10; a++ ){
+//           System.out.println(n+ " * " + a + " = "+ + a*n);
+//        }
+//
+//        for (int a=1; a<=10; a ++) {
+//            for (int b=1; b<=10; b++) {
+//                System.out.println(a+ " * " + b + " = "+ + a*b);
+//            }
+//        }
+//
+//for (int a=1; a<=10; a++) {
+//    for (int b=10; b>=1; b--) {
+//        System.out.println(a+ "." + b);
+//    }
+//}
+        int[] box = {1, 2, 3, 4, 5, 6};
+//        System.out.println(Arrays.toString(box));
+//        System.out.println(box.length);
+//for (var i =0; i<=box.length-1; i++ ) {
+//    System.out.println(box[i]);
+//}
+        for (var i = box.length-1; i >= 0; i--) {
+            System.out.println(box[i]);
+        }
+
+    }
+}

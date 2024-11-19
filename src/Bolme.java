2e020fbb9b798adbe80c820cc776315fa7b49c73
Tofq <tofiqrehimov@gmail.com>
2024-11-19 14:10:47 +0300

    new commit

--- src/Bolme.java
+++ src/Bolme.java
@@ -5,7 +5,7 @@ public class Bolme {
     public Integer Bolme (int a, String c, int b) {
         if(c=="/"){
         System.out.println(a/b);
-
+        }
         else if( b==0){
             System.out.println("Cannot divide by zero");
     }
